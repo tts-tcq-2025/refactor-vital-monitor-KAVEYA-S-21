3a44068f248ef30b4bda38580414a7d4d90c6b2e
KAVEYA-S-21 <113159193+KAVEYA-S-21@users.noreply.github.com>
2025-08-22 09:55:34 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,7 +1,3 @@
 #pragma once
 
-bool isTemperatureOk(float temperature);
-bool isPulseRateOk(float pulseRate);
-bool isSpo2Ok(float spo2);
-bool vitalsOk(float temperature, float pulseRate, float spo2);
-void alert(const char* message);
+int vitalsOk(float temperature, float pulseRate, float spo2);
